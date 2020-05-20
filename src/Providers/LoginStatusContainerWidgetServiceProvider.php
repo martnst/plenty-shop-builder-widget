d175c6ed88c3b61fd4acc28c3362b2c23ff4da78
Martin Stemmle <hi@martn.st>
2020-05-20 17:10:31 +0200

    initial commit

---
+++ src/Providers/LoginStatusContainerWidgetServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+ 
+namespace MartnWidget\Providers;
+ 
+use Plenty\Plugin\ServiceProvider;
+ 
+class LoginStatusContainerWidgetServiceProvider extends ServiceProvider
+{
+	public function register()
+	{
+  }
+ 
+	public function boot()
+  {
+  }
+}
---
+++ src/Widgets/LoginStatusContainerWidget.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace MartnWidget\Widgets;
+
+use Ceres\Widgets\Helper\BaseWidget;
+
+class LoginStatusContainerWidget extends BaseWidget
+{
+    protected $template = "MartnWidget::Widgets.LoginStatusContainerWidget";    
+
+}
